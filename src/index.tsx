3774743d59df309d67cb4cf397d81411b524ae15
yurjune <yurjune@naver.com>
2023-06-07 05:01:55 +0900

    style: 레이아웃 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import styles from './index.module.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <div className={styles.container}>
-      <App />
-    </div>
+    <App />
   </React.StrictMode>
 );
